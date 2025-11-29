feaf66d7384c8cddc8793121de2d33fc555e9b64
FadelSM <rasyahstore8888@gmail.com>
2025-11-29 16:55:59 +0700

    Enhance greeting and add personal details

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 nama = 'Fadel' #tipe data string
+usia = 16
+tinggi_badan = 166
 nama2 = 'Khonsa'
+usia2 = 16
+tinggi_badan2 = 160
 
 menyapa = 'halo semua nama gw '
-ngasihtau = ' dan temen level 2 gw yaitu '
 
-print(menyapa + nama + ngasihtau + nama2)
+print(menyapa + nama)
+print('usia saya: ' + str(usia))
+print('tinggi badan saya: ' + str(tinggi_badan))
+print('dan teman level 2 gw yaitu: ' + str(nama2))
+print('usia dia: ' + str(usia))
+print('tinggi badan dia: ' + str(tinggi_badan2))
